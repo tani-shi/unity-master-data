142c53fb1e3dfa14a5058a52e11648348d610f73
Shintaro Tanikawa <shintaro.tanikawa@gmail.com>
2020-01-02 13:14:07 +0900

    Added a class to provide some of utility menu items in Unity editor.

---
+++ Assets/UnityMasterDataDemo/Scripts/Editor/DemoMenuItems.cs
@@ -0,0 +1,34 @@
+// Copyright 2019 Shintaro Tanikawa
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using UnityEditor;
+using UnityMasterData.Editor;
+
+namespace UnityMasterDataDemo.Editor {
+    public static class DemoMenuItems {
+        [MenuItem ("UnityMasterDataDemo/Generate Master Data Class Scripts")]
+        private static void GenerateMasterDataClassScripts () {
+            MasterDataClassGenerator.GenerateAllDataScripts (
+                "Assets/UnityMasterDataDemo/Excels",
+                "Assets/UnityMasterDataDemo/Scripts",
+                "Assets/UnityMasterDataDemo/AddressableAssets",
+                "UnityMasterDataDemo");
+        }
+
+        [MenuItem ("UnityMasterDataDemo/Export Master Data Assets")]
+        private static void ExportMasterDataClass () {
+            MasterDataExporter.Export ();
+        }
+    }
+}
